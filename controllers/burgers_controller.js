2a08f2e9d73851c66c91f71f4dfdf77c7b24292f
Eli Vargas <eevargas@gmail.com>
2019-03-11 22:21:44 -0500

    Fix some stuff, added some todo comments

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,6 +2,7 @@ var express = require("express");
 var router = express.Router();
 var burger = require("../models/burger");
 
+// I change `/index` to `/`, the index route should be `/` (the root)
 router.get("/", function (req, res) {
     burger.all(function (data) {
         // eslint-disable-next-line no-console
@@ -11,6 +12,12 @@ router.get("/", function (req, res) {
 });
 
 router.post("/api/burgers", function (req, res) {
+    // TODO: here you are passing 3 parameters to the insert method...
+    // the field name, the data and the callback... but on your
+    // model you defined just 2 parameters
+    // You do not need to pass the field name here.
+    // In any case, it should be handle by the model.
+    // The controller should not deal with database details.
     burger.insert("name", req.body.name, function (result) {
         res.json({ id: result.insertId });
     });

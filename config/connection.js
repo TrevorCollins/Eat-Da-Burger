abe176639abfba9d3c94666448c0457cfd1d1a81
TrevorCollins <tcollinst1i@gmail.com>
2019-03-13 20:14:45 -0500

    Added Jaws DB to the connection

--- config/connection.js
+++ config/connection.js
@@ -8,6 +8,17 @@ var connection = mysql.createConnection({
     database: "burgers_db"
 });
 
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "rootroot",
+        database: "burgers_db"
+    });
+}
+
 connection.connect(function(err) {
     if (err) {
         // eslint-disable-next-line no-console
